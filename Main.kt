78e6ed8bf9f9c77f4db4120d17cad573eb941458
Joel Jaquez <123708276+jaq23369@users.noreply.github.com>
2024-07-18 12:46:34 -0600

    Problema2

--- Main.kt
+++ Main.kt
@@ -1,19 +1,50 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun decimalToBinary(decimal: Int): String{
-    return Integer.toBinaryString(decimal)
+fun esPalindroma(word: String): Boolean{
+    val cleanedWord = word.lowercase()
+    return cleanedWord == cleanedWord.reversed()
+}
+
+fun containsAtLeastTwoDifferentVowels(word: String): Boolean{
+    val vocales = setOf('a', 'e', 'i', 'o', 'u')
+    val vocalesEncontradas = mutableSetOf<Char>()
+    for (char in word.lowercase()) {
+        if (char in vocales) {
+            vocalesEncontradas.add(char)
+            if (vocalesEncontradas.size >= 2) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+fun empiezaConConsonante(word: String): Boolean{
+    val consonante = "bcdfghjklmnpqrstvwxyz"
+    return word.isNotEmpty() && word[0].lowercaseChar() in consonante
 }
 
 fun main(){
-    println("Ingrese un numero decimal:")
-    val input = readlnOrNull()?.toIntOrNull()
+    println("Ingresa varias palabras separadas por espacios")
+    val input = readLine()
 
-    if (input != null) {
-        val binary = decimalToBinary(input)
-        println("El numero $input en binario es: $binary")
-    }else {
-        println("Por favor, ingresa un numero valido")
+    if (input != null && input.isNotBlank()) {
+        val palabras = input.split(" ")
+        var contadorPalindromas = 0
+        var contadorPalabrasConDosVocales = 0
+        var contadorPalabrasInicianConConsonante = 0
+
+        for (word in palabras) {
+            if (esPalindroma(word)) contadorPalindromas++
+            if (containsAtLeastTwoDifferentVowels(word)) contadorPalabrasConDosVocales++
+            if (empiezaConConsonante(word)) contadorPalabrasInicianConConsonante++
+        }
+
+        println("Cantidad de palabras palíndromas: $contadorPalindromas")
+        println("Cantidad de palabras con al menos 2 vocales distintas: $contadorPalabrasConDosVocales")
+        println("Cantidad de palabras que inician con una letra consonante: $contadorPalabrasInicianConConsonante")
+    } else {
+        println("Por favor, ingrese palabras válidas.")
     }
+
 }
